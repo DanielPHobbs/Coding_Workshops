6c92b409d9c006f9ea04e54da7db2fe9ac9a422b
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2019-04-12 17:33:09 +0100

    create

---
+++ HelloWorld/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("What is your name?")
+        Dim Username = Console.ReadLine()
+        Console.WriteLine("Hello " + Username + "!")
+    End Sub
+End Module
