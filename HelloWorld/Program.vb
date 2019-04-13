e53aa5be7fe75e4aecba9ee83eb6207d7e2956d2
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2019-04-13 10:32:19 +0100

    create

--- HelloWorld/Program.vb
+++ HelloWorld/Program.vb
@@ -5,5 +5,10 @@ Module Program
         Console.WriteLine("What is your name?")
         Dim Username = Console.ReadLine()
         Console.WriteLine("Hello " + Username + "!")
+        Dim now = Date.Now
+        Dim dayOfYear = now.DayOfYear
+        Console.Write("Day of year: ")
+        Console.WriteLine(dayOfYear)
+
     End Sub
 End Module
