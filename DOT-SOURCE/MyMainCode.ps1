8420d3ed9697b46224eef514dcd824d37b8eaaa9
DanielPHobbs <Daniel.hobbs@dtekitinnovations.com>
2020-01-29 11:45:42 +0000

    Update MyMainCode.ps1

--- DOT-SOURCE/MyMainCode.ps1
+++ DOT-SOURCE/MyMainCode.ps1
@@ -3,6 +3,6 @@
 ############################################################
 
 
-. .\MyFunctions1v23.ps1
+. D:\GIT-REPOSITORY\CODING_WORKSHOP\Coding_Workshops\DOT-SOURCE\MyFunctions1v23.ps1
 
 get-help DS_RenameItem -examples
